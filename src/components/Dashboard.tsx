d972ea17932365862f3a6d45b356d0f5dff829fa
RMerkulov <fotoromario@gmail.com>
2023-10-06 00:13:57 +0300

    upd

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,191 +1,204 @@
-import {
-  Box,
-  Button,
-  FormControl,
-  MenuItem,
-  Paper,
-  Select,
-  SelectChangeEvent,
-  Table,
-  TableBody,
-  TableCell,
-  TableContainer,
-  TableHead,
-  TableRow,
-} from "@mui/material";
+import { MenuItem, Select, SelectChangeEvent } from "@mui/material";
 import { useState } from "react";
 import { Gauge, GaugeType } from "@eva-ics/webengine-react";
 
 const Dashboard = () => {
   const [selectedValue, setSelectedValue] = useState("");
-  //Test
+
   const handleChange = (event: SelectChangeEvent) => {
     setSelectedValue(event.target.value);
   };
 
-  const data_1 = Array.from({ length: 5 }, (_, rowIndex) => {
-    return [`Row ${rowIndex + 1}, Cell 1`, `Row ${rowIndex + 1}, Cell 2`];
-  });
+  const params = ["EVA ICS logal", "EVA ICS logal-1", "EVA ICS logal-2"];
+  const data = [
+    { column1: "system", column2: "mws1" },
+    { column1: "server time", column2: "2023-0615T02:14:17+02:00" },
+    { column1: "version", column2: "4.0.1" },
+    { column1: "build", column2: "2023061501" },
+    { column1: "uptime", column2: "4053.55690865" },
+  ];
 
-  const data_2 = Array.from({ length: 10 }, (_, rowIndex) => {
-    return [
-      `Row ${rowIndex + 1}, Cell 1`,
-      `Row ${rowIndex + 1}, Cell 2`,
-      `Row ${rowIndex + 1}, Cell 3`,
-      <TableCell key={`button-${rowIndex}`}>
-        <Button variant="contained" color="primary" sx={{ padding: "5x" }}>
-          kill {rowIndex + 1}
-        </Button>
-        <span>{rowIndex + 1}</span>
-      </TableCell>,
-    ];
-  });
+  const data_2 = [
+    { id: 1, col1: "Row 1", col2: "Data A", col3: "Data X", col4: "Data Z" },
+    { id: 1, col1: "Row 1", col2: "Data A", col3: "Data X", col4: "Data Z" },
+    { id: 1, col1: "Row 1", col2: "Data A", col3: "Data X", col4: "Data Z" },
+    { id: 1, col1: "Row 1", col2: "Data A", col3: "Data X", col4: "Data Z" },
+    { id: 1, col1: "Row 1", col2: "Data A", col3: "Data X", col4: "Data Z" },
+    { id: 1, col1: "Row 1", col2: "Data A", col3: "Data X", col4: "Data Z" },
+    { id: 1, col1: "Row 1", col2: "Data A", col3: "Data X", col4: "Data Z" },
+    { id: 1, col1: "Row 1", col2: "Data A", col3: "Data X", col4: "Data Z" },
+    { id: 1, col1: "Row 1", col2: "Data A", col3: "Data X", col4: "Data Z" },
+    { id: 1, col1: "Row 1", col2: "Data A", col3: "Data X", col4: "Data Z" },
+    { id: 1, col1: "Row 1", col2: "Data A", col3: "Data X", col4: "Data Z" },
+  ];
 
-  const data_3 = Array.from({ length: 10 }, (_, rowIndex) => {
-    return [
-      `Row ${rowIndex + 1}, Cell 1`,
-      `Row ${rowIndex + 1}, Cell 2`,
-      `Row ${rowIndex + 1}, Cell 3`,
-      `Row ${rowIndex + 1}, Cell 4`,
-      `Row ${rowIndex + 1}, Cell 5`,
-      `Row ${rowIndex + 1}, Cell 6`,
-    ];
-  });
+  const data_3 = [
+    {
+      id: 1,
+      col1: "Row 1",
+      col2: "Data A",
+      col3: "Data X",
+      col4: "Data Z",
+      col5: "Data Z",
+      col6: "Data Z",
+    },
+  ];
 
   return (
-    <main>
-      <div className="root-container">
-        <div className="dashboard-wrapper">
-          <div className="dashboard-wrapper__connection">
-            <div className="connection-status">Connection</div>
-            <Box sx={{ minWidth: 150 }}>
-              <FormControl fullWidth>
-                <Select
-                  labelId="dashboard-select-label"
-                  id="dashboard-select"
-                  value={selectedValue}
-                  onChange={handleChange}
-                  sx={{ backgroundColor: "transparent", border: "white" }}
-                >
-                  <MenuItem value={10}>Value 1</MenuItem>
-                  <MenuItem value={20}>Value 2</MenuItem>
-                  <MenuItem value={30}>Value 3</MenuItem>
-                </Select>
-              </FormControl>
-            </Box>
+    <div className="root-container">
+      <div className="dashboard-main-wrapper">
+        <div className="dashboard-select">
+          <div className="dashboard-select__connection">Connection</div>
+          <div>
+            <Select
+              className="editors-select"
+              value={selectedValue}
+              onChange={handleChange}
+            >
+              {params?.map((v) => (
+                <MenuItem key={v} value={v}>
+                  {v}
+                </MenuItem>
+              ))}
+            </Select>
           </div>
-          <div className="dashboard-wrapper__content">
-            <div className="dashboard-wrapper__block">
-              <div className="dashboard-wrapper__part">
-                <TableContainer component={Paper}>
-                  <Table>
-                    <TableHead>
-                      <TableRow>
-                        <TableCell sx={{ padding: "0px" }}>Column 1</TableCell>
-                        <TableCell>Column 2</TableCell>
-                      </TableRow>
-                    </TableHead>
-                    <TableBody sx={{ padding: "0px" }}>
-                      {data_1.map((row, rowIndex) => (
-                        <TableRow key={rowIndex} sx={{ padding: "0px" }}>
-                          {row.map((cell, cellIndex) => (
-                            <TableCell key={cellIndex}>{cell}</TableCell>
-                          ))}
-                        </TableRow>
-                      ))}
-                    </TableBody>
-                  </Table>
-                </TableContainer>
+        </div>
+        <div className="dashboard-main-wrapper-content">
+          <div className="dashboard-main-wrapper-content__side-left">
+            <div className="dashboard-main-content-block_table">
+              <div className="dashboard-main-content-block__header">
+                <h2 className="heading-h2">Node Info</h2>
               </div>
-              <div className="dashboard-wrapper__part">
-                <TableContainer component={Paper} style={{ maxHeight: 300 }}>
-                  <Table>
-                    <TableHead>
-                      <TableRow>
-                        <TableCell>Column 1</TableCell>
-                        <TableCell>Column 2</TableCell>
-                        <TableCell>Column 3</TableCell>
-                        <TableCell>Column 4</TableCell>
-                      </TableRow>
-                    </TableHead>
-                    <TableBody>
-                      {data_2.map((row, rowIndex) => (
-                        <TableRow key={rowIndex}>
-                          {row.map((cell, cellIndex) => (
-                            <TableCell key={cellIndex}>{cell}</TableCell>
-                          ))}
-                        </TableRow>
-                      ))}
-                    </TableBody>
-                  </Table>
-                </TableContainer>
+              <div className="dashboard-main-content-block__content content-info">
+                <table className="info-table">
+                  <tbody>
+                    {data.map((row, index) => (
+                      <tr
+                        key={index}
+                        style={{
+                          backgroundColor:
+                            index % 2 === 0 ? "#191D22" : "#121721",
+                        }}
+                      >
+                        <td>{row.column1}</td>
+                        <td>{row.column2}</td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
               </div>
             </div>
-            <div className="dashboard-wrapper__block">
-              <div className="dashboard-wrapper__part">
-                <div className="dashboard-wrapper__content__gauges">
-                  <Gauge
-                    type={GaugeType.Modern}
-                    diameter={130}
-                    minValue={0}
-                    maxValue={100}
-                  />
-                  <Gauge
-                    type={GaugeType.Modern}
-                    diameter={130}
-                    minValue={0}
-                    maxValue={100}
-                  />
-                  <Gauge
-                    type={GaugeType.Modern}
-                    diameter={130}
-                    minValue={0}
-                    maxValue={100}
-                  />
-                  <Gauge
-                    type={GaugeType.Modern}
-                    diameter={130}
-                    minValue={0}
-                    maxValue={100}
-                  />
-                  <Gauge
-                    type={GaugeType.Modern}
-                    diameter={130}
-                    minValue={0}
-                    maxValue={100}
-                  />
-                </div>
+            <div className="dashboard-main-content-block_table">
+              <div className="dashboard-main-content-block__header">
+                <h2 className="heading-h2">HMI sessions</h2>
               </div>
-              <div className="dashboard-wrapper__part">
-                <TableContainer component={Paper} style={{ maxHeight: 300 }}>
-                  <Table>
-                    <TableHead>
-                      <TableRow>
-                        <TableCell>Column 1</TableCell>
-                        <TableCell>Column 2</TableCell>
-                        <TableCell>Column 3</TableCell>
-                        <TableCell>Column 4</TableCell>
-                        <TableCell>Column 5</TableCell>
-                        <TableCell>Column 6</TableCell>
-                      </TableRow>
-                    </TableHead>
-                    <TableBody>
-                      {data_3.map((row, rowIndex) => (
-                        <TableRow key={rowIndex}>
-                          {row.map((cell, cellIndex) => (
-                            <TableCell key={cellIndex}>{cell}</TableCell>
-                          ))}
-                        </TableRow>
-                      ))}
-                    </TableBody>
-                  </Table>
-                </TableContainer>
+              <div className="dashboard-main-content-block__content content-sessions">
+                <table className="sessions-table">
+                  <thead>
+                    <tr>
+                      <th>Column 1</th>
+                      <th>Column 2</th>
+                      <th>Column 3</th>
+                      <th>Column 4</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {data_2.map((row, index) => (
+                      <tr
+                        key={row.id}
+                        style={{
+                          backgroundColor:
+                            index % 2 === 0 ? "#191D22" : "#121721",
+                        }}
+                      >
+                        <td>{row.col1}</td>
+                        <td>{row.col2}</td>
+                        <td>{row.col3}</td>
+                        <td className="table-action">
+                          {row.col4}
+                          <button>kill</button>
+                        </td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
+          <div className="dashboard-main-wrapper-content__side-right">
+            <div className="dashboard-main-content-gauges">
+              <Gauge
+                type={GaugeType.Modern}
+                diameter={120}
+                minValue={0}
+                maxValue={100}
+              />
+              <Gauge
+                type={GaugeType.Modern}
+                diameter={120}
+                minValue={0}
+                maxValue={100}
+              />
+              <Gauge
+                type={GaugeType.Modern}
+                diameter={120}
+                minValue={0}
+                maxValue={100}
+              />
+              <Gauge
+                type={GaugeType.Modern}
+                diameter={120}
+                minValue={0}
+                maxValue={100}
+              />
+              <Gauge
+                type={GaugeType.Modern}
+                diameter={120}
+                minValue={0}
+                maxValue={100}
+              />
+            </div>
+            <div className="dashboard-main-content-block_table">
+              <div className="dashboard-main-content-block__header">
+                <h2 className="heading-h2">Cloud</h2>
+              </div>
+              <div className="dashboard-main-content-block__content content-sessions">
+                <table className="sessions-table">
+                  <thead>
+                    <tr>
+                      <th>Column 1</th>
+                      <th>Column 2</th>
+                      <th>Column 3</th>
+                      <th>Column 4</th>
+                      <th>Column 5</th>
+                      <th>Column 6</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {data_2.map((row, index) => (
+                      <tr
+                        key={row.id}
+                        style={{
+                          backgroundColor:
+                            index % 2 === 0 ? "#191D22" : "#121721",
+                        }}
+                      >
+                        <td>{row.col1}</td>
+                        <td>{row.col2}</td>
+                        <td>{row.col3}</td>
+                        <td>{row.col4}</td>
+                        <td>{row.col4}</td>
+                        <td>{row.col4}</td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
               </div>
             </div>
           </div>
         </div>
       </div>
-    </main>
+    </div>
   );
 };
 
