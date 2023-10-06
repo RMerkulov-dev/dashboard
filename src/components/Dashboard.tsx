82dd8bce6a4de4ac17ec05a625eda818e1fbc709
RMerkulov <fotoromario@gmail.com>
2023-10-06 22:07:22 +0300

    finish

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,6 +1,7 @@
 import { MenuItem, Select, SelectChangeEvent } from "@mui/material";
 import { useState } from "react";
 import { Gauge, GaugeType } from "@eva-ics/webengine-react";
+import { data, data_2, params } from "../data";
 
 const Dashboard = () => {
   const [selectedValue, setSelectedValue] = useState("");
@@ -8,29 +9,6 @@ const Dashboard = () => {
   const handleChange = (event: SelectChangeEvent) => {
     setSelectedValue(event.target.value);
   };
-
-  const params = ["EVA ICS logal", "EVA ICS logal-1", "EVA ICS logal-2"];
-  const data = [
-    { column1: "system", column2: "mws1" },
-    { column1: "server time", column2: "2023-0615T02:14:17+02:00" },
-    { column1: "version", column2: "4.0.1" },
-    { column1: "build", column2: "2023061501" },
-    { column1: "uptime", column2: "4053.55690865" },
-  ];
-
-  const data_2 = [
-    { id: 1, col1: "Row 1", col2: "Data A", col3: "Data X", col4: "Data Z" },
-    { id: 1, col1: "Row 1", col2: "Data A", col3: "Data X", col4: "Data Z" },
-    { id: 1, col1: "Row 1", col2: "Data A", col3: "Data X", col4: "Data Z" },
-    { id: 1, col1: "Row 1", col2: "Data A", col3: "Data X", col4: "Data Z" },
-    { id: 1, col1: "Row 1", col2: "Data A", col3: "Data X", col4: "Data Z" },
-    { id: 1, col1: "Row 1", col2: "Data A", col3: "Data X", col4: "Data Z" },
-    { id: 1, col1: "Row 1", col2: "Data A", col3: "Data X", col4: "Data Z" },
-    { id: 1, col1: "Row 1", col2: "Data A", col3: "Data X", col4: "Data Z" },
-    { id: 1, col1: "Row 1", col2: "Data A", col3: "Data X", col4: "Data Z" },
-    { id: 1, col1: "Row 1", col2: "Data A", col3: "Data X", col4: "Data Z" },
-    { id: 1, col1: "Row 1", col2: "Data A", col3: "Data X", col4: "Data Z" },
-  ];
 
   return (
     <div>
---
+++ src/data/index.tsx
@@ -0,0 +1,22 @@
+export const params = ["EVA ICS logal", "EVA ICS logal-1", "EVA ICS logal-2"];
+export const data = [
+  { column1: "system", column2: "mws1" },
+  { column1: "server time", column2: "2023-0615T02:14:17+02:00" },
+  { column1: "version", column2: "4.0.1" },
+  { column1: "build", column2: "2023061501" },
+  { column1: "uptime", column2: "4053.55690865" },
+];
+
+export const data_2 = [
+  { id: 1, col1: "Row 1", col2: "Data A", col3: "Data X", col4: "Data Z" },
+  { id: 1, col1: "Row 1", col2: "Data A", col3: "Data X", col4: "Data Z" },
+  { id: 1, col1: "Row 1", col2: "Data A", col3: "Data X", col4: "Data Z" },
+  { id: 1, col1: "Row 1", col2: "Data A", col3: "Data X", col4: "Data Z" },
+  { id: 1, col1: "Row 1", col2: "Data A", col3: "Data X", col4: "Data Z" },
+  { id: 1, col1: "Row 1", col2: "Data A", col3: "Data X", col4: "Data Z" },
+  { id: 1, col1: "Row 1", col2: "Data A", col3: "Data X", col4: "Data Z" },
+  { id: 1, col1: "Row 1", col2: "Data A", col3: "Data X", col4: "Data Z" },
+  { id: 1, col1: "Row 1", col2: "Data A", col3: "Data X", col4: "Data Z" },
+  { id: 1, col1: "Row 1", col2: "Data A", col3: "Data X", col4: "Data Z" },
+  { id: 1, col1: "Row 1", col2: "Data A", col3: "Data X", col4: "Data Z" },
+];
